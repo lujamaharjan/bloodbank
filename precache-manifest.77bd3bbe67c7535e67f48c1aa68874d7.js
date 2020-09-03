ec20f244c2704964a049a97729da561ff336cbae
Sachin Maharjan <sachinmaharjan@acd.edu.np>
2020-09-03 17:29:19 +0545

    Updates

---
+++ precache-manifest.77bd3bbe67c7535e67f48c1aa68874d7.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c1f0d767b673a272faecfa5271cf87",
+    "url": "/bloodbank/index.html"
+  },
+  {
+    "revision": "4f2144a2607815bd4fc6",
+    "url": "/bloodbank/static/css/2.d5f72184.chunk.css"
+  },
+  {
+    "revision": "24cfe4a9fed4c9e4ba60",
+    "url": "/bloodbank/static/css/main.a40ffd9a.chunk.css"
+  },
+  {
+    "revision": "4f2144a2607815bd4fc6",
+    "url": "/bloodbank/static/js/2.937c934b.chunk.js"
+  },
+  {
+    "revision": "aaf578ca74bee50053d87df7f5eafd71",
+    "url": "/bloodbank/static/js/2.937c934b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24cfe4a9fed4c9e4ba60",
+    "url": "/bloodbank/static/js/main.73fb02bb.chunk.js"
+  },
+  {
+    "revision": "d908ff40ad2f045ff04e",
+    "url": "/bloodbank/static/js/runtime-main.8d118a31.js"
+  },
+  {
+    "revision": "1b569c97c5551dbad996dc179213a810",
+    "url": "/bloodbank/static/media/Avatar_Panda-512.1b569c97.png"
+  },
+  {
+    "revision": "62e3c77c100f6ba6c3f25bffd46dc3ed",
+    "url": "/bloodbank/static/media/about.62e3c77c.jpg"
+  },
+  {
+    "revision": "f1dceaedbe9b487f2e1f2044123d9581",
+    "url": "/bloodbank/static/media/blog1.f1dceaed.png"
+  },
+  {
+    "revision": "d5c0a390708fb8f0f4fde2410e46576f",
+    "url": "/bloodbank/static/media/blog3.d5c0a390.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/bloodbank/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/bloodbank/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/bloodbank/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/bloodbank/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/bloodbank/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/bloodbank/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "8e98818a6b8db62e4adeb456de977f86",
+    "url": "/bloodbank/static/media/header-bg.8e98818a.jpg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/bloodbank/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/bloodbank/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/bloodbank/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/bloodbank/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/bloodbank/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/bloodbank/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/bloodbank/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/bloodbank/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/bloodbank/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/bloodbank/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
